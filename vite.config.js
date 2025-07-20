45ffa15d3b54ce179498c714d51d26051d8a60a6
srijana <b180008@nitsikkim.ac.in>
2025-07-20 12:57:53 +0530

    pages commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-     base:"/"
+     base:"/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        // Add more pages if needed
+        // about: resolve(__dirname, 'about.html'),
+        // team: resolve(__dirname, 'team.html'),
+      },
+    },
+  },   
 })
